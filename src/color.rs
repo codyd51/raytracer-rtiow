06ad37d7af2e01bc1d9bf605525383b8819eac92
Phillip Tennen <phillip@axleos.com>
2024-03-16 17:04:50 +0000

    Implement Color and Pos semantic wrappers around Vec3

---
+++ src/color.rs
@@ -0,0 +1,20 @@
+use std::fmt::{Display, Formatter};
+use std::ops::Deref;
+use crate::vec3::Vec3;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct Color(Vec3);
+
+impl Deref for Color {
+    type Target = Vec3;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!("Col({}, {}, {})", self.0.x, self.0.y, self.0.z))
+    }
+}
---
+++ src/pos.rs
@@ -0,0 +1,20 @@
+use std::fmt::{Display, Formatter};
+use std::ops::Deref;
+use crate::vec3::Vec3;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct Pos(Vec3);
+
+impl Deref for Pos {
+    type Target = Vec3;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Display for Pos {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!("Pos({}, {}, {})", self.0.x, self.0.y, self.0.z))
+    }
+}
